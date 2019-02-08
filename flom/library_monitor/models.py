4dcba5f5b9010aa6ecae1cab7771e56f4dc3a35e
kaijianZ <zhongk@rpi.edu>
2019-02-08 17:14:29 -0500

    create models for hardware inputs

--- flom/library_monitor/models.py
+++ flom/library_monitor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Room(models.Model):
+    room_id = models.IntegerField()
+    key = models.CharField(max_length=200)
+
+
+class Occupancy(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    occupied = models.BooleanField()
+
