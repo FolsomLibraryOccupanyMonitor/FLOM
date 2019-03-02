15192e0a2968824cd5afe19bf369d109873e058f
jingfei <zhouj5@rpi.edu>
2019-03-02 06:07:11 -0500

    Update send.py

--- _hardware/send.py
+++ _hardware/send.py
@@ -1,29 +1,29 @@
 import requests
+import sys
 ADDR = "http://127.0.0.1:8000/library_monitor/"
 
 def update(op, room):
-	# Make a get request to get the latest position of the international space station from the opennotify api.
 	if op == 'enter':
-		response = requests.get(ADDR+room+"/enter")
+		# Make a get request to get the latest position of the international space station from the opennotify api.
+		response = requests.get(ADDR+str(room)+"/enter")
 		# Print the status code of the response.
 		print(response.text)
 	elif op == 'leave':
-		response = requests.get(ADDR+room+"/enter")
+		response = requests.get(ADDR+str(room)+"/leave")
 		print(response.text)
 	elif op == 'check':
-		response = requests.get(ADDR+room)
+		response = requests.get(ADDR+str(room))
 		print(response.text)
 	else:
 		print('Invalid Input')
 
-
-print('Available Input: [enter/leave/check] [room number/floor number]')
-
-while True:
-	uin = input('-------\n')
-	oplist = uin.split(' ')
-	# print(oplist)
-	if len(uin)==0:
-		break
-	else:
-		update(oplist[0],oplist[1])
+if sys.argv[1]=='test':
+	print('Available Input: [enter/leave/check] [room number/floor number]')
+	while True:
+		uin = input('-------\n')
+		oplist = uin.split(' ')
+		# print(oplist)
+		if len(uin)==0:
+			break
+		else:
+			update(oplist[0],oplist[1])
