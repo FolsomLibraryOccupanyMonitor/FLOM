9b026795597218da2205a0dc8c2c3acd93f2d511
kaijianZ <zhongk@rpi.edu>
2019-03-12 16:00:13 -0400

    finished a demo template page

--- _software/library_monitor/views.py
+++ _software/library_monitor/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.core.cache import cache
 from django.template import loader
-from django.shortcuts import render
-import json
 
 
 # Create your views here.
