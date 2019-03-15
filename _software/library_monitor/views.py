5bb18250598c551520bed5beb934269ac627ff90
kaijianZ <zhongk@rpi.edu>
2019-03-14 20:10:57 -0400

    add the check hash

--- _software/library_monitor/views.py
+++ _software/library_monitor/views.py
@@ -1,24 +1,33 @@
 from django.http import HttpResponse
 from django.core.cache import cache
 from django.template import loader
+from django.http import Http404
 import datetime
-from _software.library_monitor.models import Log
+from library_monitor.models import Log
 
 
 def flr3(request):
     return render(request, 'library_monitor/floor3.html')
 
 
-def enter(request, room_id):
+def enter(request, room_id, secret_key):
+
+    if secret_key != 'ok':
+        return HttpResponse('You are not one of us!')
+
     cache.set(room_id, datetime.datetime.now(), None)
     return HttpResponse(f"You're entering room {room_id}.")
 
 
-def leave(request, room_id):
-    enter_time = cache.get(room_id)
+def leave(request, room_id, secret_key):
 
-    if enter_time is None:
-        return HttpResponse('wut?')
+    if secret_key != 'ok':
+        return HttpResponse('You are not one of us!')
+
+    try:
+        enter_time = cache.get(room_id)
+    except enter_time is None:
+        raise Http404("Nobody is in the room!")
 
     Log.objects.create(room_id=room_id, enter_time=enter_time,
                        leave_time=datetime.datetime.now())
