598daba5e0a83b1438162999bbcd2b49e9c05f4a
YoungChul Chun <youngcc3157@gmail.com>
2019-03-21 17:45:20 -0400

    created basic database for both rooms and logs

--- _software/database/models.py
+++ _software/database/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
-class RoomInfo(models.Model):
-	col = models.CharField(max_length=10)
+class Room(models.Model):
+	room_name = models.CharField('Room Name', max_length=20, null = True, default = "", editable = False)
+	room_floor = models.CharField('Room floor', max_length=20, null = True, default = "", editable = False)
+
+class Log(models.Model):
+	room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+	enter_time = models.DateTimeField('Enter Time')
+	leave_time = models.DateTimeField('Leave Time')
