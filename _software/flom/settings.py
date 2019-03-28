881ba3e9432ccd8f73097ca5f0e39204eff16439
Jingfei Zhou <jingfei1024@gmail.com>
2019-03-28 03:13:37 -0400

    allow www.flom.ml

--- _software/flom/settings.py
+++ _software/flom/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['flom.ml','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['flom.ml','www.flom.ml','localhost','127.0.0.1']
 
 
 # Application definition
