d8a1a7d36a4f70ebefab474d4d420cb32ae74294
prashb99 <prashb@rpi.edu>
2019-04-09 16:35:40 -0400

    added developers to about page

--- _software/flom/settings.py
+++ _software/flom/settings.py
@@ -38,7 +38,7 @@ with open(CONFIG_FILE) as f:
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['flom.ml','www.flom.ml','localhost','127.0.0.1']
 
