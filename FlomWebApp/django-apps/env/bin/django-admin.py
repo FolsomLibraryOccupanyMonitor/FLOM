cee80de5b0f0274b645f9f75f2d03bfd1d2e7072
Emmanuel Sylvester <emmanuel.sylvester22@gmail.com>
2019-07-16 13:55:17 -0400

    Set up Django Server

---
+++ FlomWebApp/django-apps/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/Sylvee/Documents/RCOS/FLOM/FlomWebApp/django-apps/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
