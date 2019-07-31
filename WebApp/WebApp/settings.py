8d8a92c1118d3a5a42beef5fd9e640a5c1840a31
Jiawen <linj14@rpi.edu>
2019-08-01 02:59:30 +0800

    update settings.py

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'WebApp.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'flom',
+            'USER': 'flom_admin',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
