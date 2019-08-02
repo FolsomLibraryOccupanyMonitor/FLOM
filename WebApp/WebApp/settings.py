2c4229d8f8d8f052b52c1b03f006cbc6228511e8
Jason-Durr <durrj2@rpi.edu>
2019-08-01 23:16:48 -0400

    Got the picture to load for floor 3

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -120,4 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'floor3/images/static'),
+    os.path.join(BASE_DIR, 'floor4/images/static')
 ]
