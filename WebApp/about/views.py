ba495b29bf455b04895a860b2550738016efe1f3
barnesv17 <50973809+barnesv17@users.noreply.github.com>
2019-08-15 16:04:49 -0300

    Update comments

--- WebApp/about/views.py
+++ WebApp/about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
-# Create your views here.
+# Create your views here. A view is a Python function that takes a web request and returns a web response.
 
+# Returns the display for the about page
 def index(request):
 	return render_to_response('about/templates/html/about.html')
 
