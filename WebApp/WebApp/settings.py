c27559edbc8967995f57c8dabe621b163ca41eb0
Jack Cherella <jcherella10@gmail.com>
2019-08-15 22:29:37 -0400

    Added some comments to various files

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Add directories for new pages here
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'floor3/static'),
