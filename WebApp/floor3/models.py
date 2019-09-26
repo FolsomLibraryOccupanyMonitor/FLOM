4f74cafae1e0f67eada741e057a5781bf91dbef9
Thomas Hopkins <thomashopkins000@gmail.com>
2019-09-26 13:11:12 -0400

    updated models documentation

--- WebApp/floor3/models.py
+++ WebApp/floor3/models.py
@@ -4,10 +4,11 @@ class Room(models.Model):
 	'''
 	Model for a Room
 	@member (s)
-		int roomID - the number of the room (ex. 301, 324)
+		string roomID - the number of the room (ex. 301, 324)
 		int occupied - occupation status of the room (0 = false, 1 = true)
-		Date lastExited - date of last exit
-		Date lastEntered - date of last entry
+		DateTime lastExited - date of last exit
+		DateTime lastEntered - date of last entry
+		string roomType - group or single room
 	'''
 	roomID = models.CharField(max_length = 5) # ID of room (Ex. 301, 324)
 	occupied = models.IntegerField() # 0 for empty, 1 for occupied
