8da42bb0dab3bbde19c9eecfae98ad073c98e186
Thomas Hopkins <thomashopkins000@gmail.com>
2019-10-03 15:41:22 -0400

    pulled from latest push

--- WebApp/stats/views.py
+++ WebApp/stats/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render_to_response
-from stats.models import Room
-from stats.models import OccupancyStats
-from stats.models import RoomUsage
+from stats.models import Room, OccupancyStats, RoomUsage
 from datetime import datetime
 
 
