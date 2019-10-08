b691ee25c87c8ffab6005493d19a5f8a9e1c64d2
JustinChoiRPI2022 <choij9@rpi.edu>
2019-10-08 00:30:13 -0400

    Part 1 of some arduino code

--- WebApp/hardware/occupancy/occupancy.ino
+++ WebApp/hardware/occupancy/occupancy.ino
@@ -1,22 +1,51 @@
-#define PIR_PIN 2 //connect pir sensor to pin 2 of Arduino
+#include <stdlib.h>
+#include <unistd.h>
 
-void setup_pins(){
-    pinMode(PIR_PIN, INPUT);
-}
+#include <hive_map.h>
+
+#define PIR = 2; //connect PIR sensor to pin 2 of arduino
+#define LED = 13; //connect LED for testing
+
+int PIR_STATE = LOW;
+int val = 0;
+
+typedef struct Occupancy_Space { //creating an occupancy space
+    HiveMapSpaceId space;
+    bool isOccupied;
+} Occupancy_Space;
+
+HiveMapNode(Occupancy_Space) node; //room defined by occupancy space
 
 void setup(){
-    Serial.begin(9600); //begin serial monitor
+    pinMode(LED, OUTPUT);
+    pinMode(PIR, INPUT);
+    Serial.begin(9600);
 }
 
 void loop(){
-    int val = digitalRead(PIR_PIN);
-    Serial.println(val);
-    if (val == 1) {
-        Serial.println("Motion detected");
-    }
+    node.loc = 10; //room number
+    node.goal_loc = 1; //floor number
+    node.state_received = NULL;
+    val = digitalRead(PIR);
+    if (val == HIGH) {            // check if the input is HIGH
+        digitalWrite(LED, HIGH);  // turn LED ON
+        if (PIR_STATE == LOW) {
+            Serial.println("Motion detected!");
+            pirState = HIGH;
+            node.isOccupied = true;
+        }
+    } 
     else {
-        Serial.println("No motion");
+        digitalWrite(ledPin, LOW); // turn LED OFF
+        if (pirState == HIGH){
+            Serial.println("Motion ended!");
+            pirState = LOW;
+            node.isOccupied = false;
+        }
     }
-
-
 }
+
+
+
+
+
