5a9fd1ad990aefaaf7a9c2aafa1c19e7ec1f810d
JustinChoiRPI2022 <choij9@rpi.edu>
2019-10-08 15:00:09 -0400

    Arduino code modified

--- WebApp/hardware/occupancy/occupancy.ino
+++ WebApp/hardware/occupancy/occupancy.ino
@@ -19,27 +19,27 @@ HiveMapNode(Occupancy_Space) node; //room defined by occupancy space
 void setup(){
     pinMode(LED, OUTPUT);
     pinMode(PIR, INPUT);
+    node.loc = 10; //room number
+    node.goal_loc = 1; //floor number
+    node.state_received = NULL;
     Serial.begin(9600);
 }
 
 void loop(){
-    node.loc = 10; //room number
-    node.goal_loc = 1; //floor number
-    node.state_received = NULL;
     val = digitalRead(PIR);
     if (val == HIGH) {            // check if the input is HIGH
         digitalWrite(LED, HIGH);  // turn LED ON
         if (PIR_STATE == LOW) {
             Serial.println("Motion detected!");
-            pirState = HIGH;
+            PIR_STATE = HIGH;
             node.isOccupied = true;
         }
     } 
     else {
         digitalWrite(ledPin, LOW); // turn LED OFF
-        if (pirState == HIGH){
+        if (PIR_STATE == HIGH){
             Serial.println("Motion ended!");
-            pirState = LOW;
+            PIR_STATE = LOW;
             node.isOccupied = false;
         }
     }
