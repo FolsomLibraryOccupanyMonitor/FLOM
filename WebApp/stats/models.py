1b12188ec436951d724307cbb629b5075dd5f063
prashb99 <prashb@rpi.edu>
2019-10-18 20:25:12 -0400

    moving time module

--- WebApp/stats/models.py
+++ WebApp/stats/models.py
@@ -17,21 +17,3 @@ class statsLog(models.Model):
 	roomID = models.CharField(max_length = 5)
 	timeStamp = models.DateTimeField(auto_now_add=True, editable=False)
 
-class TimeFrame(models.Model):
-	'''
-	A TimeFrame is a representation of the usage statistics that have
-	been gathered within the span of the designated time.
-	@member (s)
-		date - date on which TimeFrame was created
-		span - interval in which the statistics were gathered
-				eg. "day", "week", "month", "year"
-		roomID - individual room this applies to
-		totalOccupants - total number of occupants over the span
-		avgOccLength - average occupation length for a room (in minutes)
-	'''
-	date = models.DateField()
-	roomPointer = models.ForeignKey(Room, on_delete=models.CASCADE)
-	span = models.CharField(max_length=6)
-	roomID = models.CharField(max_length=5)
-	totalOccupants = models.IntegerField(default=0)
-	avgOccLength = models.IntegerField(default=0)
--- WebApp/timers/models.py
+++ WebApp/timers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from floor.models import Room
 # Create your models here.
+class TimeFrame(models.Model):
+	'''
+	A TimeFrame is a representation of the usage statistics that have
+	been gathered within the span of the designated time.
+	@member (s)
+		date - date on which TimeFrame was created
+		span - interval in which the statistics were gathered
+				eg. "day", "week", "month", "year"
+		roomID - individual room this applies to
+		totalOccupants - total number of occupants over the span
+		avgOccLength - average occupation length for a room (in minutes)
+	'''
+	date = models.DateField()
+	roomPointer = models.ForeignKey(Room, on_delete=models.CASCADE)
+	span = models.CharField(max_length=6)
+	roomID = models.CharField(max_length=5)
+	totalOccupants = models.IntegerField(default=0)
+	avgOccLength = models.IntegerField(default=0)
