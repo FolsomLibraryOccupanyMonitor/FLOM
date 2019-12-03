889d9147e74541c697472cd19b43f711014dac65
ChrisReed0114 <reedc7@rpi.edu>
2019-12-03 17:14:24 -0500

    Check rooms that don't exist

--- WebApp/floor/tests.py
+++ WebApp/floor/tests.py
@@ -16,4 +16,19 @@ class BasicTestCase(TestCase):
     #Exit Room
     response = c.get("http://127.0.0.1:8000/floor/exit/3/311/pass") #Response is of type byte
     response_string = (response.content).decode('ASCII') #To String
-    assert(response_string == "Room successfully entered!")
+    assert(response_string == "Room successfully exited!")
+
+# Create your tests here.
+class WrongRoom(TestCase):
+    #Enter Room Success
+    #Must Login to enter the room
+    c = Client()
+    c.login(username='admin', password='admin')
+    #Enter Room
+    response = c.get("http://127.0.0.1:8000/floor/enter/3/300/pass") #Response is of type byte
+    response_string = (response.content).decode('ASCII') #To String
+    assert(response_string == "Room Not Found")
+    #Exit Room
+    response = c.get("http://127.0.0.1:8000/floor/exit/3/300/pass") #Response is of type byte
+    response_string = (response.content).decode('ASCII') #To String
+    assert(response_string == "Room Not Found")
