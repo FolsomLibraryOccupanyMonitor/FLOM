1147b3aa5315610e3a5a3936809603f315e8df70
ChrisReed0114 <reedc7@rpi.edu>
2019-12-03 16:57:59 -0500

    Experimenting making get requests

--- WebApp/floor/tests.py
+++ WebApp/floor/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . import views
+from django.test import Client
+from django.http import HttpResponse
 
 # Create your tests here.
+class BasicTestCase(TestCase):
+    #Testing a get request
+    c = Client()
+    c.login(username='admin', password='admin')
+    response = c.get("http://127.0.0.1:8000/floor/enter/3/311/pass")
+    response_string = (response.content).decode('ASCII')
+    print("Get:", response_string)
