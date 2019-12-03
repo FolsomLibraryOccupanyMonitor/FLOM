7d3b1922b8743753241dc51181f9a12feb6307b9
ChrisReed0114 <reedc7@rpi.edu>
2019-12-03 17:10:08 -0500

    Made first test case

--- WebApp/floor/tests.py
+++ WebApp/floor/tests.py
@@ -5,9 +5,15 @@ from django.http import HttpResponse
 
 # Create your tests here.
 class BasicTestCase(TestCase):
-    #Testing a get request
+    #Enter Room Success
+    #Must Login to enter the room
     c = Client()
     c.login(username='admin', password='admin')
-    response = c.get("http://127.0.0.1:8000/floor/enter/3/311/pass")
-    response_string = (response.content).decode('ASCII')
-    print("Get:", response_string)
+    #Enter Room
+    response = c.get("http://127.0.0.1:8000/floor/enter/3/311/pass") #Response is of type byte
+    response_string = (response.content).decode('ASCII') #To String
+    assert(response_string == "Room successfully entered!")
+    #Exit Room
+    response = c.get("http://127.0.0.1:8000/floor/exit/3/311/pass") #Response is of type byte
+    response_string = (response.content).decode('ASCII') #To String
+    assert(response_string == "Room successfully entered!")
