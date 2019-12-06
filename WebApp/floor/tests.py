6b82829a8ce58cc133ef48a7d39ea5f47b91a33f
ChrisReed0114 <reedc7@rpi.edu>
2019-12-06 17:00:58 -0500

    Final of tests

--- WebApp/floor/tests.py
+++ WebApp/floor/tests.py
@@ -1,3 +1,6 @@
+import ast
+import json
+
 from django.test import TestCase
 from . import views
 from django.test import Client
@@ -38,3 +41,47 @@ class WrongRoom(TestCase):
     response_string = (response.content).decode('ASCII') #To String
     assert(response_string == "Room Not Found")
     print("Room Not Found")
+
+# Create your tests here.
+class DoneAlready(TestCase):
+    print("Trying to enter and exit multiple times back to back")
+    #Enter Room Success
+    #Must Login to enter the room
+    c = Client()
+    c.login(username='admin', password='admin')
+    #Enter Room
+    response = c.get("http://127.0.0.1:8000/floor/enter/3/311/pass") #Response is of type byte
+    response = c.get("http://127.0.0.1:8000/floor/enter/3/311/pass") #Response is of type byte
+    response_string = (response.content).decode('ASCII') #To String
+    assert(response_string == "Room already occupied")
+    print("Room already occupied")
+    #Exit Room
+    response = c.get("http://127.0.0.1:8000/floor/exit/3/311/pass") #Response is of type byte
+    response = c.get("http://127.0.0.1:8000/floor/exit/3/311/pass") #Response is of type byte
+    response_string = (response.content).decode('ASCII') #To String
+    assert(response_string == "Room already empty")
+    print("Room already empty")
+
+
+class AllRooms(TestCase):
+    print("Going to enter and exit every room")
+    #Load all of the rooms
+    CONFIG_FILE = 'WebApp/config.ini'
+    config = {}
+    with open(CONFIG_FILE) as cfile:
+        config = json.load(cfile)
+    #Login to exit and enter
+    c = Client()
+    c.login(username='admin', password='admin')
+    #Loop through all of the floors
+    for floor in config["FLOORS"]:
+        rooms = config["FLOORS"][floor]
+        rooms = ast.literal_eval(rooms)
+        print("On floor:", floor)
+        #Loop through all of the rooms
+        for room in rooms:
+            #Enter the room
+            response = c.get("http://127.0.0.1:8000/floor/enter/{}/{}/pass".format(floor,room))
+            #Exit the room
+            response = c.get("http://127.0.0.1:8000/floor/exit/{}/{}/pass".format(floor,room))
+        print("All Rooms successfully exited and entered!!!")
